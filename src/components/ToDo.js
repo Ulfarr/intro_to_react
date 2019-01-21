485a84ccae1940a16ae58cf8c04abc2f4bde2895
Adam Moran <pipboy.moran9@gmail.com>
2019-01-21 14:54:28 -0600

    checkpoint 7 work

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
         return (
-          <li>a todo will go here</li>  
+          <li>{ this.props.description }</li>
+          <li>
+          <input type="checkbox" checked={ this.props.isCompleted } />
+          <span>{ this.props.description }</span>
+        </li>  
         );
       }
 }
