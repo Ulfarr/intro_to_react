2998a488cb31637fc6076cb9558f885924bdb1a0
Ulfarr <44230194+Ulfarr@users.noreply.github.com>
2019-01-28 11:21:14 -0600

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-
-
+ 
 class ToDo extends Component {
     render() {
         return (
           <li>
-          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-          <span>{ this.props.description }{''}</span>
-          <button onClick={() => this.props.onDelete(this.props.id)}>X</button>
-        </li>  
+            <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+            <span>{ this.props.description }</span>
+            <button onClick={() => this.props.onDelete(this.props.id)}>Remove Todo</button>
+          </li>
         );
-    }
+      }
 }
  
 export default ToDo;
